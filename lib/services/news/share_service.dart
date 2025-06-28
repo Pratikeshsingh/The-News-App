3b4d99037ebf505b04f6b917ab945fc119596231
Pratikesh Singh <42940293+Pratikeshsingh@users.noreply.github.com>
2025-06-28 11:00:31 +0200

    Replace esys_flutter_share with share_plus

--- lib/services/news/share_service.dart
+++ lib/services/news/share_service.dart
@@ -8,23 +8,34 @@ import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 
 // Package imports:
-import 'package:esys_flutter_share/esys_flutter_share.dart';
+import 'dart:io';
+
+import 'package:share_plus/share_plus.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
 
 // Project imports:
 import 'package:inshort_clone/controller/provider.dart';
 
 void convertWidgetToImageAndShare(BuildContext context, containerKey) async {
-  RenderRepaintBoundary renderRepaintBoundary =
-      containerKey.currentContext.findRenderObject();
-  ui.Image boxImage = await renderRepaintBoundary.toImage(pixelRatio: 1);
-  ByteData byteData = await boxImage.toByteData(format: ui.ImageByteFormat.png);
-  Uint8List uInt8List = byteData.buffer.asUint8List();
+  final RenderRepaintBoundary renderRepaintBoundary =
+      containerKey.currentContext.findRenderObject() as RenderRepaintBoundary;
+  final ui.Image boxImage =
+      await renderRepaintBoundary.toImage(pixelRatio: 1);
+  final ByteData byteData =
+      await boxImage.toByteData(format: ui.ImageByteFormat.png);
+  final Uint8List uInt8List = byteData.buffer.asUint8List();
+
   try {
-    await Share.file(
-        'imsanjaysoni/InshortClone', 'inshortClone.png', uInt8List, 'image/png',
-        text:
-            'This message sent from *inshorts Clone* made by *Sanjay Soni*\nFork this repository on *Github*\n\n https://github.com/imSanjaySoni/Inshorts-Clone.');
+    final tempDir = await getTemporaryDirectory();
+    final file = await File('${tempDir.path}/inshortClone.png').create();
+    await file.writeAsBytes(uInt8List);
+
+    await Share.shareXFiles([
+      XFile(file.path,
+          mimeType: 'image/png',
+          name: 'inshortClone.png'),
+    ], text: 'This message sent from *inshorts Clone* made by *Sanjay Soni*\nFork this repository on *Github*\n\n https://github.com/imSanjaySoni/Inshorts-Clone.');
   } catch (e) {
     print('error: $e');
   }
