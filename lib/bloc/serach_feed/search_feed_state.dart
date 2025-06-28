6ba9ce7b088a5ff18870c2a36864c690251fe93d
Pratikesh Singh <42940293+Pratikeshsingh@users.noreply.github.com>
2025-06-28 10:37:55 +0200

    fix equality props in bloc states

--- lib/bloc/serach_feed/search_feed_state.dart
+++ lib/bloc/serach_feed/search_feed_state.dart
@@ -9,26 +9,24 @@ abstract class SearchFeedState extends Equatable {}
 
 class SearchFeedInitialState extends SearchFeedState {
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [];
 }
 
 class SearchFeedLoadingState extends SearchFeedState {
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [];
 }
 
 class SearchFeedLoadedState extends SearchFeedState {
   final List<Articles> news;
   SearchFeedLoadedState({@required this.news});
-  get moviesList => news;
-
   @override
-  List<Object> get props => null;
+  List<Object?> get props => [news];
 }
 
 class SearchFeedErrorState extends SearchFeedState {
   final String message;
   SearchFeedErrorState({@required this.message});
   @override
-  List<Object> get props => null;
+  List<Object?> get props => [message];
 }
